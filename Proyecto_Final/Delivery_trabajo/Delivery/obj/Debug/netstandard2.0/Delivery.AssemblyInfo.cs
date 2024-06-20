8d572998b19f0c874605c565e283c525f8f5994b
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-06-19 22:28:07 -0500

    Eliminacion de datos

--- Proyecto_Final/Delivery_trabajo/Delivery/obj/Debug/netstandard2.0/Delivery.AssemblyInfo.cs
+++ Proyecto_Final/Delivery_trabajo/Delivery/obj/Debug/netstandard2.0/Delivery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4cb02c63fd54130f55900f6f2c5228bd18f6cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db5abbf7fb5c564568c8e57736e95edeed98f2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Proyecto_Final/Tiktok_replica_trabajo/README.md
+++
@@ -1,4 +0,0 @@
-###### PANTALLAZO DEL PROYECTO
-![](https://i.ibb.co/McmqWcv/erwe345.png)
-###### TUTORIAL PASO A PASO EN https://codigo369.com/
-![](https://i.ibb.co/McmqWcv/erwe345.png)
